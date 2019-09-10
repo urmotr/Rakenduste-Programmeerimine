b3d9ceb37d764be08374e8ad10d2ea71d0d7809b
urmotr <urmotr@tlu.ee>
2019-09-10 18:40:07 +0300

    test

--- warmup/server.js
+++ warmup/server.js
@@ -3,12 +3,13 @@ const app = express()
 const path = require("path");
 const PORT = 3000
 
-app.get('/', (req, res) => (
-  res.sendFile(path.resolve(__dirname, "static", "index.html"))
-))
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+})
 
-app.use(express.static("static"));
+app.use(express.static('static'));
 
+// Heroku needs process.env.PORT
 app.listen(process.env.PORT || PORT, () => {
   console.log("Server started", PORT);
 });
