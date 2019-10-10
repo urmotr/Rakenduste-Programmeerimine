4c81231c63cf4c4305c092a0b328d47b26d0de26
urmotr <urmotr@tlu.ee>
2019-10-10 12:51:56 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     port: 9000,
-    open: 'chrome',
+    open: 'firefox',
     proxy: {
       "/api": "http://localhost:3000"
     }
