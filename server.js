1140035680568fdfa914f7446bd04aec71e6c90b
urmotr <urmotr@tlu.ee>
2019-10-10 13:19:37 +0300

    heroku

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const path = require("path");
-const PORT = 3000
+const PORT = 3000;
+const DB = require("./database.js");
 
-app.get('/items/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+app.get("/api/items", (req, res) => {
+  res.json(DB.getItems());
+});
+
+app.get("/api/items/:itemId", (req, res ) => {
+  res.send(DB.getItem(req.params.itemId));
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.get('/items/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
 
 app.use(express.static('dist'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     port: 9000,
-    open: 'firefox',
+    //open: 'firefox',
     proxy: {
       "/api": "http://localhost:3000"
     }
