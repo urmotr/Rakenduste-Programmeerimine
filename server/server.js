66bd659cc0cbc64967e2198d6afd416ca356f873
urmotr <urmotr@tlu.ee>
2019-10-17 13:23:19 +0300

    mongo start

--- server/server.js
+++ server/server.js
@@ -3,6 +3,36 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+var kittySchema = mongoose.Schema({
+  name: String
+});
+
+var Kitten = new mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+  name: "red cat"
+});
+
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-c7y2s.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL)
+    .then(()=>{
+      console.log("Database access success!");
+      kitten1.save( err => {
+        if(err){
+          console.error("We have an error")
+        } else {
+          console.log("Saved")
+        }
+      })
+    })
+    .catch(err => {
+      console.error("error happened", err)
+    });
 
 app.get("/api/items", (req, res) => {
   res.json(DB.getItems());
